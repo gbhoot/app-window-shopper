7a621c462db048c4d67a39473ce05b6f262031e2
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-07-05 11:23:01 -0700

    added currency symbol based on location to text fields

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -12,7 +12,18 @@ import UIKit
 class CurrencyTxtField: UITextField {
 
     override func draw(_ rect: CGRect) {
-//        let size: CGFloat
+        let size: CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y:  (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 0.6049185739)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
     }
     
     override func prepareForInterfaceBuilder() {
@@ -36,6 +47,7 @@ class CurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
