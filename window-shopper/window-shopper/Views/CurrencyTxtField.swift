1d6ac257d605ded19f476b5e9def46d3866a6c4b
Gurpreet Bhoot <gur_boo15@Gurpreets-MacBook-Pro.local>
2018-07-05 11:49:41 -0700

    attempted to change format of custom text fields for currency

--- window-shopper/window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/window-shopper/Views/CurrencyTxtField.swift
@@ -45,14 +45,23 @@ class CurrencyTxtField: UITextField {
     
     func customizeView() {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
-        layer.cornerRadius = 5.0
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         textAlignment = .center
+        layer.cornerRadius = 5.0
         clipsToBounds = true
+        
+//        let numFormat = NumberFormatter()
+//        numFormat.maximumFractionDigits = 2
+//        numFormat.maximumFractionDigits = 2
+//        numFormat.usesGroupingSeparator = true
+//        numFormat.numberStyle = .currency
+//        numFormat.locale = .current
+//        let amount = NSAttributedString(string: <#T##String#>)
+//        attributed
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
             attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
     }
 
